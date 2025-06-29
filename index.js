da9da4f25d55566c48708a23b3596b7aad0b0bd3
Collins Opiayo <opiayocollins2@gmail.com>
2025-06-29 18:56:44 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Your code here
+// Cat Class
+class Cat {
+  constructor(name, breed) {
+    this.name = name;
+    this.breed = breed;
+  }
+
+  speak() {
+    return `${this.name} says meow!`;
+  }
+}
+
+// Dog Class
+class Dog {
+  constructor(name, breed) {
+    this.name = name;
+    this.breed = breed;
+  }
+
+  speak() {
+    return `${this.name} says woof!`;
+  }
+}
+
+// Bird Class (Corrected)
+class Bird {
+  constructor(name, species) {
+    this.name = name;
+    this.species = species;
+  }
+
+  speak() {
+    if (this.species.toLowerCase() === 'parrot') {
+      return `It's me! ${this.name}, the parrot!`;
+    } else {
+      return `${this.name} says squawk!`;
+    }
+  }
+}
+
+
+
+
